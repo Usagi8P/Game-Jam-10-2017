c89b8a5fa4175765dd34c763d87cf4696b0e1b85
Philipp Stein <ph7stein@hotmail.com>
2017-10-22 02:38:48 +0100

    timer

---
+++ Assets/Scripts/MasterVolume.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MasterVolume : MonoBehaviour {
+
+    public AudioMixer masterMixer;
+
+    public void SetVolume(float VolumeLvl)
+    {
+        masterMixer.SetFloat("Volume", VolumeLvl);
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    //string timer;
+
+    public Text sliderValue;
+    public Slider slider;
+
+    void Update ()
+    {
+         sliderValue.text = slider.value.ToString("0.0");
+    }
+}
