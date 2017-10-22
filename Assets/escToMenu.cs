ccdc2dfb74010bb6e2d3e6aea6bdc80c15a64347
GreyEzzey <greyezzey@gmail.com>
2017-10-22 02:43:26 +0100

    Menu push

---
+++ Assets/escToMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class escToMenu : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+            print("space key was pressed");
+
+    }
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
+
+
+
+public class changeScene : MonoBehaviour
+{
+
+    
+}
